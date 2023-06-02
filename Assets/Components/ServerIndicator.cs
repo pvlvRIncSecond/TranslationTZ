537cfc4547f9f950c18b55924dcdf85650f16181
pvlvRInc <pra_dev@mail.ru>
2023-06-02 14:41:19 +0500

    Reconnection text

--- Assets/Components/ServerIndicator.cs
+++ Assets/Components/ServerIndicator.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Timers;
 using Infrastructure.Services.Progress;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,20 +9,48 @@ namespace Components
     public class ServerIndicator : MonoBehaviour
     {
         [SerializeField] private Image _indicator;
+        [SerializeField] private TMPro.TextMeshProUGUI _reconnectText;
         private IPersistentProgress _progress;
 
-        private void OnDestroy() => 
+        private void OnDestroy() =>
             _progress.OnConnectedChanged -= SwitchIndicator;
 
         public void Construct(IPersistentProgress progress)
         {
             _progress = progress;
             _progress.OnConnectedChanged += SwitchIndicator;
+            _progress.OnConnectedChanged += AnimateText;
+
+            _reconnectText.alpha = 0;
         }
 
-        public void SwitchIndicator()
+        private void AnimateText()
         {
-            _indicator.color = _progress.ConnectedToServer ? new Color(0.27f, 0.75f, 0.17f) : new Color(0.67f, 0.17f, 0.15f);
+            if (_progress.ConnectedToServer)
+            {
+                StopAllCoroutines();
+                _reconnectText.alpha = 0;
+            }
+            else
+            {
+                StartCoroutine(AnimateAlpha());
+            }
+        }
+
+        private IEnumerator AnimateAlpha()
+        {
+            while (true)
+            {
+                _reconnectText.alpha = (Mathf.Sin(Time.time) + 1) / 2;
+                yield return null;
+            }
+        }
+
+        private void SwitchIndicator()
+        {
+            _indicator.color = _progress.ConnectedToServer
+                ? new Color(0.27f, 0.75f, 0.17f)
+                : new Color(0.67f, 0.17f, 0.15f);
             _indicator.GraphicUpdateComplete();
         }
     }
